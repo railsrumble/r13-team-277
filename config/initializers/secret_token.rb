966b8295adc301dacf6448c7846d870bd8de376c
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-10-18 23:34:34 -0700

    new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsrumble::Application.config.secret_key_base = '3ebf893926d47e3d82109b6f46b8e8834ca010a1d80e1c6be984dd3d7a75d9ba669a315d7005315b3a9364d0e7e50492c31a274ef18fb21b7e03cb37c187485a'
