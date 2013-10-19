6ce884e0a8b1b2a5045d23873497382a72a15dd6
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-10-19 12:11:12 -0700

    Add home controller with welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Railsrumble::Application.routes.draw do
+  get "home/index"
   resources :tasks
   
-  root :to => "tasks#index"
+  root :to => "home#index"
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
