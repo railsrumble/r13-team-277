28cff742f96c0b3dadad5bf2c367e2e7c1caf317
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-10-20 16:07:43 -0700

    Sort stickies

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-var toDoApp = angular.module('toDoApp', ['ngResource']);
+var toDoApp = angular.module('toDoApp', ['ngResource', 'ui.sortable']);
 toDoApp.config([
   "$httpProvider", function($httpProvider) {
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
