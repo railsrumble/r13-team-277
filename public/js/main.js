11a6abe38db6d6a2b74952da8fc50d609cec98af
Harsimran Singh Maan <maan.harry@gmail.com>
2013-10-20 10:47:16 -0700

    Fix angular model for csrf

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,7 @@
 'use strict';
 var toDoApp = angular.module('toDoApp', ['ngResource']);
+toDoApp.config([
+  "$httpProvider", function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  }
+]);
