8c44855519921ddb27517363f20c97ea763b31c0
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-10-20 16:47:19 -0700

    Truncate task length to 60

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,21 @@
 class Task < ActiveRecord::Base
+  include ActionView::Helpers::TextHelper
   include TaskType
+  
   belongs_to :user
   belongs_to :category
 
   validates_presence_of :user, :description
+  
+  before_save :truncate_description
 
   def mark_completed
   	self.completed = true
   	save
+  end
+  
+  def truncate_description
+    self.description = truncate(self.description, :length => 60)
   end
   
   def category_name
